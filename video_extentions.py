f3ad6c8dbbd8c5f9c27ab87ce9132dcb73c2447c
gitJaesik <maguire1815@gmail.com>
2024-04-30 14:35:42 +0900

    Create video_extentions.py

---
+++ video_extentions.py
@@ -0,0 +1,30 @@
+# 다양한 비디오 파일 확장자 목록
+video_extensions = [
+    ".avi",  # Audio Video Interleave
+    ".mp4",  # MPEG-4 Part 14
+    ".mov",  # QuickTime File Format
+    ".wmv",  # Windows Media Video
+    ".flv",  # Flash Video Format
+    ".mkv",  # Matroska Video
+    ".m4v",  # MPEG-4 Video File
+    ".mpg",  # MPEG Video File
+    ".mpeg", # MPEG Movie
+    ".rm",   # RealMedia File
+    ".rmvb", # RealMedia Variable Bitrate
+    ".vob",  # DVD Video Object
+    ".asf",  # Advanced Systems Format
+    ".3gp",  # 3GPP Multimedia File
+    ".3g2",  # 3GPP2 Multimedia File
+    ".f4v",  # Flash MP4 Video File
+    ".m2ts", # MPEG-2 Transport Stream
+    ".ts",   # MPEG Transport Stream
+    ".mts",  # AVCHD Video File
+    ".divx", # DivX-Encoded Movie File
+    ".webm", # WebM Video File
+    ".ogv",  # Ogg Video File
+    ".ogg",  # Ogg Media File
+]
+
+# 확장자 목록 출력
+for ext in video_extensions:
+    print(ext)
